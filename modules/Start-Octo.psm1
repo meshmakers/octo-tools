741915f4e3990284921cd1a1b56d84e6a35fc99b
reimarklammer <reimar.klammer@gmail.com>
2023-07-31 12:02:45 +0200

    fix typo

--- modules/Start-Octo.psm1
+++ modules/Start-Octo.psm1
@@ -1,6 +1,5 @@
-function Start-Octo()
-{
-<#
+function Start-Octo() {
+    <#
 .SYNOPSIS
 Starts all Octo Mesh services. 
 
@@ -13,8 +12,8 @@ If set to $true, the Bot Service will be started. If set to $false, it will not 
 .PARAMETER identityService
 If set to $true, the Identity Service will be started. If set to $false, it will not be started.
 
-.PARAMETER assetRepService
-If set to $true, the Asset Rep Service will be started. If set to $false, it will not be started.
+.PARAMETER assetRepoService
+If set to $true, the Asset Repo Service will be started. If set to $false, it will not be started.
 
 .PARAMETER timeSeriesRepService
 If set to $true, the Time Series Rep Service will be started. If set to $false, it will not be started.
@@ -37,19 +36,18 @@ This example starts all services except for the Bot Service.
 Use this function to selectively start Octo Mesh services based on your requirements.
 #>
 
-param(
-    [Parameter()] [Boolean]$botService=$true,
-    [Parameter()] [Boolean]$identityService=$true,
-    [Parameter()] [Boolean]$assetRepService=$true,
-    [Parameter()] [Boolean]$timeSeriesRepService=$true,
-    [Parameter()] [Boolean]$communicationControllerService=$true,
-    [Parameter()] [Boolean]$adminPanel=$true,
-    [Parameter()] [Boolean]$identityOnly=$false  
-)
-    if ($identityOnly)
-    {
+    param(
+        [Parameter()] [Boolean]$botService = $true,
+        [Parameter()] [Boolean]$identityService = $true,
+        [Parameter()] [Boolean]$assetRepoService = $true,
+        [Parameter()] [Boolean]$timeSeriesRepService = $true,
+        [Parameter()] [Boolean]$communicationControllerService = $true,
+        [Parameter()] [Boolean]$adminPanel = $true,
+        [Parameter()] [Boolean]$identityOnly = $false  
+    )
+    if ($identityOnly) {
         $botService = $false;
-        $assetRepService = $false;
+        $assetRepoService = $false;
         $timeSeriesRepService = $false;
         $communicationControllerService = $false;
         $adminPanel = $false;
@@ -59,44 +57,36 @@ param(
     $jobs = New-Object System.Collections.ArrayList
     $publishVersion = "net7.0"
 
-    if (!(Test-Path $rootPath))
-    {
+    if (!(Test-Path $rootPath)) {
         Write-Error "Root path $rootPath does not exist"
         return;
     }
 
-    function Start-Service($workingDirectory, $cmd, $logname, $cmdArguments, $jobName, $aspnetEnvironment = "Development")
-    {
+    function Start-Service($workingDirectory, $cmd, $logname, $cmdArguments, $jobName, $aspnetEnvironment = "Development") {
         Write-Host "Starting $( $jobName ) -> $cmdArguments"
-        $arguments = @([System.IO.Path]::Combine($rootPath, $workingDirectory),[System.IO.Path]::Combine($rootPath, $logDir, $logname), $aspnetEnvironment)
+        $arguments = @([System.IO.Path]::Combine($rootPath, $workingDirectory), [System.IO.Path]::Combine($rootPath, $logDir, $logname), $aspnetEnvironment)
         $arguments += $cmdArguments
         $job = Start-Job -ScriptBlock { Set-Location $args[0]; $env:ASPNETCORE_ENVIRONMENT = $args[2]; $localArgs = $args | Select-Object -Skip 3; & "$input" $localArgs 2>&1 >> $args[1] } -InputObject $cmd -ArgumentList $arguments -Name $jobName
         $jobs.Add($job) | OUT-NULL;
     }
 
-    function Get-ServiceStatus()
-    {
-        foreach ($job in $jobs)
-        {
+    function Get-ServiceStatus() {
+        foreach ($job in $jobs) {
             Write-Host "Status job $( $job.Name ): $( $job.State )"
 
         }
     }
 
-    function Delete-LogFile([string]$file)
-    {
+    function Delete-LogFile([string]$file) {
         $file = [System.IO.Path]::Combine($rootPath, $logDir, $file)
-        if (Test-Path $file)
-        {
+        if (Test-Path $file) {
             Remove-Item -Force $file
         }
     }
 
-    function Create-LogDirectory()
-    {
+    function Create-LogDirectory() {
         $path = [System.IO.Path]::Combine($rootPath, $logDir);
-        If (!(test-path $path))
-        {
+        If (!(test-path $path)) {
             New-Item -ItemType Directory -Force -Path $path | Out-Null
         }
     }
@@ -106,8 +96,8 @@ param(
     $env:OCTO_BOT_LOGDIR = "../../../../logs/bot/"
     $env:OCTO_HISTORIAN_LOGDIR = "../../../../logs/historian/"
     $env:OCTO_POLICY_LOGDIR = "../../../../logs/historian/"
-    $env:OCTO_SYSTEM__ADMINUSERPASSWORD="OctoAdmin1"
-    $env:OCTO_SYSTEM__DATABASEUSERPASSWORD="OctoUser1"
+    $env:OCTO_SYSTEM__ADMINUSERPASSWORD = "OctoAdmin1"
+    $env:OCTO_SYSTEM__DATABASEUSERPASSWORD = "OctoUser1"
 
 
     Create-LogDirectory
@@ -119,28 +109,22 @@ param(
     Delete-LogFile -file "BotServices.log"
     Delete-LogFile -file "AdminPanel.log"
     
-    if ($identityService)
-    {
+    if ($identityService) {
         Start-Service -workingDirectory "octo-identity-services/bin/Debug/IdentityServices/$publishVersion/publish/" -cmd "dotnet" -logname "IdentityServices.log" -cmdArguments @("Meshmakers.Octo.Backend.IdentityServices.dll", "--urls=https://*:5003/") -jobName "IdentityServices"
     }
-    if ($assetRepService)
-    {
+    if ($assetRepoService) {
         Start-Service -workingDirectory "octo-asset-repo-services/bin/Debug/AssetRepositoryServices/$publishVersion/publish/" -cmd "dotnet" -logname "AssetRepositoryServices.log" -cmdArguments @("Meshmakers.Octo.Backend.AssetRepositoryServices.dll", "--urls=http://localhost:5000;https://localhost:5001") -jobName "AssetRepositoryServices"
     }
-    if ($timeSeriesRepService)
-    {
+    if ($timeSeriesRepService) {
         Start-Service -workingDirectory "octo-time-series-repo-services/bin/Debug/HistorianRepositoryServices/$publishVersion/publish/" -cmd "dotnet" -logname "TimeSeriesRepositoryServices.log" -cmdArguments @("Meshmakers.Octo.Backend.HistorianRepositoryServices.dll", "--urls=https://localhost:5013") -jobName "TimeSeriesRepositoryServices"
     }
-    if ($botService)
-    {
+    if ($botService) {
         Start-Service -workingDirectory "octo-bot-services/bin/Debug/BotServices/$publishVersion/publish/" -cmd "dotnet" -logname "BotServices.log" -cmdArguments @("Meshmakers.Octo.Backend.BotServices.dll", "--urls=https://localhost:5009") -jobName "BotServices"
     }
-    if ($communicationControllerService)
-    {
+    if ($communicationControllerService) {
         Start-Service -workingDirectory "octo-communication-controller-services/bin/Debug/CommunicationControllerServices/$publishVersion/publish/" -cmd "dotnet" -logname "CommunicationControllerServices.log" -cmdArguments @("Meshmakers.Octo.Backend.CommunicationControllerServices.dll", "--urls=https://localhost:5015") -jobName "CommunicationControllerServices"
     }
-    if ($adminPanel)
-    {
+    if ($adminPanel) {
         Start-Service -workingDirectory "octo-frontend-admin-panel/bin/Debug/AdminPanel/$publishVersion/publish/" -cmd "dotnet" -logname "AdminPanel.log" -cmdArguments @("Meshmakers.Octo.Backend.AdminPanel.dll", "--urls=https://localhost:5005") -jobName "AdminPanel" -aspnetEnvironment "Staging"
     }
 
@@ -149,11 +133,9 @@ param(
     Write-Host "Started. Press key to exit"
 
     $wait = $true;
-    do
-    {
+    do {
         # wait for a key to be available:
-        if ([Console]::KeyAvailable)
-        {
+        if ([Console]::KeyAvailable) {
             # read the key, and consume it so it won't
             # be echoed to the console:
             [Console]::ReadKey($true) | Out-Null
@@ -161,10 +143,8 @@ param(
             break
         }
 
-        foreach ($job in $jobs)
-        {
-            if ($job.State -ne "Running")
-            {
+        foreach ($job in $jobs) {
+            if ($job.State -ne "Running") {
                 Write-Warning "Service $( $job.Name ) is in status $( $job.State )"
                 Receive-Job $job | Write-Output
                 $wait = $false
@@ -178,8 +158,7 @@ param(
 
 
     Write-Host "Exiting jobs"
-    foreach ($job in $jobs)
-    {
+    foreach ($job in $jobs) {
         Write-Host "Stopping job $( $job.Name )"
         $job.StopJob()
     }
