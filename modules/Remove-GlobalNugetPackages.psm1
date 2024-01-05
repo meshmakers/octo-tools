544b67439c5f0556caeb79c9fb71576db7e1cdc7
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-01-05 11:59:16 +0100

    Added script to remove nuget packages for dev environment

---
+++ modules/Remove-GlobalNugetPackages.psm1
@@ -0,0 +1,25 @@
+
+function Remove-GlobalNugetPackages
+{
+    if (!(Test-Path $globalNugetPackagesPath)) {
+        Write-Error "Path $globalNugetPackagesPath does not exist"
+        return;
+    }
+
+    Write-Host "Searching in $globalNugetPackagesPath..."
+
+    $allDirectories = Get-ChildItem -Directory -Path $globalNugetPackagesPath -Filter "meshmakers.*"
+
+    foreach ($directory in $allDirectories) {     
+        Write-Host "Searching at $directory"
+        $packageDirectories = Join-Path $directory '999.0.0'
+     
+        if ((Test-Path $packageDirectories)) {
+
+            Write-Host "Deleting $packageDirectories" -ForegroundColor Red
+            Remove-Item -Path $packageDirectories -Recurse -Force
+        }
+    }
+}
+
+Export-ModuleMember -Function @('Remove-GlobalNugetPackages')
