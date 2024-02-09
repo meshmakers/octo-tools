07ee522fec0406031f2813ffefae9b30e08d93a4
Reimar Klammer <reimar.klammer@meshmakers.io>
2024-02-09 13:58:01 +0100

    Add summary of git pull

--- modules/Sync-AllGitRepos.psm1
+++ modules/Sync-AllGitRepos.psm1
@@ -7,17 +7,33 @@ function Sync-AllGitRepos {
 
     # Get all directories starting with "octo-"
     $octoDirectories = Get-ChildItem -Directory -Path $rootPath -Filter "octo-*"
-
+    $status = @{}
     foreach ($directory in $octoDirectories) {
         $gitDirectory = Join-Path -Path $directory.FullName -ChildPath ".git"
 
         # Check if the ".git" directory exists
         if (Test-Path -Path $gitDirectory -PathType Container) {
-            Write-Host "Pulling git repository $($directory.FullName)" -ForegroundColor Green
-            Sync-GitRepo $directory.FullName
+            try {
+                Write-Host "Pulling git repository $($directory.FullName)" -ForegroundColor Green
+                Sync-GitRepo $directory.FullName
+                $status.Add($directory.FullName, $true)
+            }
+            catch {
+                Write-Host "Error pulling git repository $($directory.FullName)" -ForegroundColor Red
+                $status.Add($directory.FullName, $false)
+            }
         }
     }
     
+    foreach($key in $status.Keys) {
+        if($status[$key] -eq $true) {
+            Write-Host "Pulling repository ${key} was successful.)" -ForegroundColor Green
+        }else {
+            Write-Host "${key}: $($status[$key]) failed." -ForegroundColor Red
+        }
+    }
+
+    Write-Host " "
     Write-Host "Done"
     Write-Host "To sync all submodules use 'Sync-AllSubmodules'"
 }
