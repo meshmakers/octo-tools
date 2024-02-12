067a930bd43180a6672e8951f6ed4bf5ec6beebd
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-02-12 23:05:01 +0100

    Updates of image versions of infrastructure Containers

--- modules/Invoke-OctoCliLoginLocal.psm1
+++ modules/Invoke-OctoCliLoginLocal.psm1
@@ -2,7 +2,7 @@ function Invoke-OctoCliLoginLocal
 {
     param($tenantId = "meshtest")
 
-    octo-cli -c Config -asu "https://localhost:5001/" -isu "https://localhost:5003/" -bsu "https://localhost:5009/" -tid $tenantId
+    octo-cli -c Config -asu "https://localhost:5001/" -isu "https://localhost:5003/" -bsu "https://localhost:5009/" -csu "https://localhost:5015/" -tid $tenantId
     octo-cli -c Login -i
 }
 
