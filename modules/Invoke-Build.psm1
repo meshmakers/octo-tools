3601983ff12c97153503d6b065b8de90f30d05fa
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-08-06 11:30:12 +0200

    Fixes in scripts - logs are created now by file at Invoke-BuildAll Better handling if build fails, also in publish.

--- modules/Invoke-Build.psm1
+++ modules/Invoke-Build.psm1
@@ -3,10 +3,21 @@ function Invoke-Build {
         [string]$configuration = "Release",
         [string]$repositoryPath = ".\"
     )
+    $logFile = Join-Path $repositoryPath "Invoke-Build.log"
+    if (Test-Path $logFile) {
+        Remove-Item $logFile
+    }
     
     Write-Host "[$configuration] Building git repository $repositoryPath" -ForegroundColor Green
-    dotnet build $repositoryPath -c $configuration
-    $global:LASTEXITCODE = $LASTEXITCODE
+    dotnet build $repositoryPath -c $configuration > $logFile
+    $state = $LASTEXITCODE -eq 0
+    if ($state -eq $false) {
+        Write-Host "[$configuration] Build failed" -ForegroundColor Red
+    }
+    else {
+        Write-Host "[$configuration] Build finished" -ForegroundColor Green
+    }
+    $Global:LASTEXITCODE = $LASTEXITCODE
 }
 
 
