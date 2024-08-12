d794d11a8895d7006b3654bfec1e0218d6f4f25a
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-08-12 18:20:00 +0200

    Added script for removing a cluster from local k8 configuration

--- modules/profile.ps1
+++ modules/profile.ps1
@@ -60,6 +60,7 @@ Import-Module "$modulePath/Invoke-KillDotnet.psm1"
 Import-Module "$modulePath/Remove-BinAndObjFolders.psm1"
 Import-Module "$modulePath/Get-AllGitRepStatus.psm1"
 Import-Module "$modulePath/Join-KubeConfigs.psm1"
+Import-Module "$modulePath/Remove-KubeConfig.psm1"
 
 
 if (!(Test-Path $nugetPath)) {
