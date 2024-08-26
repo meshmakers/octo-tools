21fec514c3a524f809d85017b2fd9f04f3b0b629
Lukas Stoiber <lukas.stoiber@gmx.net>
2024-08-26 12:09:51 +0200

    fix path to build submodules

--- modules/Invoke-BuildFrontend.psm1
+++ modules/Invoke-BuildFrontend.psm1
@@ -1,52 +1,59 @@
 function Invoke-BuildFrontend {
     
     # rm -rf node_modules in "octo-frontend-libraries" and "octo-frontend-admin-panel"
-    $frontendLibrariesPath = Join-Path $rootPath "octo-frontend-admin-panel\src\octo-frontend-libraries\FrontendLibraries\ClientApp"
+    $frontendLibrariesPath = Join-Path $rootPath "octo-frontend-admin-panel\src\octo-frontend-libraries\src\FrontendLibraries\ClientApp"
     $frontendAdminPanelPath = Join-Path $rootPath "octo-frontend-admin-panel\src\AdminPanel\ClientApp"
-    
+
+    Write-Host "Build frontend libraries in $frontendLibrariesPath" 
     
     $frontendLibrariesNodeModulesPath =  Join-Path $frontendLibrariesPath "node_modules"
-    Write-Host "Delete node_modules in $frontendLibrariesNodeModulesPath"
     if (Test-Path $frontendLibrariesNodeModulesPath) {
+        Write-Host "Delete node_modules in $frontendLibrariesNodeModulesPath" 
         Remove-Item -Path $frontendLibrariesNodeModulesPath -Recurse -Force
     }
     
     # npm install in "octo-frontend-libraries"
-    Write-Host "npm install in $frontendLibrariesPath"
+
     if (Test-Path $frontendLibrariesPath) {
+        Write-Host "npm install in $frontendLibrariesPath" 
         Push-Location $frontendLibrariesPath
         npm install
         Pop-Location
     }
     
     # npm run build in "octo-frontend-libraries"
-    Write-Host "npm run build in $frontendLibrariesPath"
     if (Test-Path $frontendLibrariesPath) {
+        Write-Host "npm run build in $frontendLibrariesPath" 
+        
         Push-Location $frontendLibrariesPath
         npm run build
         Pop-Location
     }
-    
-    
+
+
+    Write-Host "Build frontend libraries in $frontendAdminPanelPath" 
     
     # rm -rf node_modules in "octo-frontend-admin-panel"
     $frontendAdminPanelNodeModulesPath = Join-Path $frontendAdminPanelPath "node_modules"
-    Write-Host "Delete node_modules in $frontendAdminPanelNodeModulesPath"
     if (Test-Path $frontendAdminPanelNodeModulesPath) {
+        Write-Host "Delete node_modules in $frontendAdminPanelNodeModulesPath"  
+        
         Remove-Item -Path $frontendAdminPanelNodeModulesPath -Recurse -Force
     }
 
     # npm install in "octo-frontend-admin-panel"
-    Write-Host "npm install in $frontendAdminPanelPath"
     if (Test-Path $frontendAdminPanelPath) {
+        Write-Host "npm install in $frontendAdminPanelPath" 
+        
         Push-Location $frontendAdminPanelPath
         npm install
         Pop-Location
     }
 
     # npm run build in "octo-frontend-admin-panel"
-    Write-Host "npm run build in $frontendAdminPanelPath"
     if (Test-Path $frontendAdminPanelPath) {
+        Write-Host "npm run build in $frontendAdminPanelPath" 
+        
         Push-Location $frontendAdminPanelPath
         npm run build
         Pop-Location
