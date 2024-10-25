d7d1ef720e0e57e588d8f0866c7c71111d79b651
Lukas Stoiber <38109506+TurboLuke@users.noreply.github.com>
2024-10-25 16:02:11 +0200

    Log npm & node version in build frontend script (#3)
    * Log npm & node version in build frontend script
    
    * remove blank

--- modules/Invoke-BuildFrontend.psm1
+++ modules/Invoke-BuildFrontend.psm1
@@ -1,4 +1,10 @@
 function Invoke-BuildFrontend {
+
+    $npmVersion = npm --version
+    $nodeVersion = node --version
+    
+    Write-Host "npm version: $npmVersion"
+    Write-Host "node version: $nodeVersion"
     
     $frontendAdminPanelRootPath = Join-Path $rootPath "octo-frontend-admin-panel"
     $frontendLibrariesPath = Join-Path $rootPath "octo-frontend-admin-panel\src\octo-frontend-libraries\src\FrontendLibraries\ClientApp"
