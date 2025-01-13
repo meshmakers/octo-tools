25737ce3f1d2790d3913a8c7ae1f987df6f38cf4
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-01-13 10:36:38 +0100

    AB#1677: Added communication services scopes to debug configuration

--- modules/Invoke-SetDebugConfiguration.psm1
+++ modules/Invoke-SetDebugConfiguration.psm1
@@ -6,12 +6,8 @@ function Invoke-SetDebugConfiguration
     octo-cli -c AddScopeToClient --clientid octo-admin-panel-debug --name "systemAPI.full_access"
     octo-cli -c AddScopeToClient --clientid octo-admin-panel-debug --name "identityAPI.full_access"
     octo-cli -c AddScopeToClient --clientid octo-admin-panel-debug --name "botAPI.full_access"
-
-
-    octo-cli -c AddAuthorizationCodeClient --clienturi https://localhost:5005/ --clientid octo-admin-panel --redirectUri "https://localhost:5005/" --name "Admin Panel"
-    octo-cli -c AddScopeToClient --clientid octo-admin-panel-debug --name "systemAPI.full_access"
-    octo-cli -c AddScopeToClient --clientid octo-admin-panel-debug --name "identityAPI.full_access"
-    octo-cli -c AddScopeToClient --clientid octo-admin-panel-debug --name "botAPI.full_access"
+    octo-cli -c AddScopeToClient --clientid octo-admin-panel-debug --name "communicationSystemAPI.full_access"
+    octo-cli -c AddScopeToClient --clientid octo-admin-panel-debug --name "communicationTenantAPI.full_access"
 }
 
 Export-ModuleMember -Function @('Invoke-SetDebugConfiguration')
