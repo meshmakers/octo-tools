c08f7b8742b1b21282b9676d8713ef4e5a2bbfc0
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-03-16 17:05:17 +0100

    AB#1860: New: Added report service uri

--- modules/Invoke-OctoCliLoginLocal.psm1
+++ modules/Invoke-OctoCliLoginLocal.psm1
@@ -2,7 +2,7 @@ function Invoke-OctoCliLoginLocal
 {
     param($tenantId = "meshtest")
 
-    octo-cli -c Config -asu "https://localhost:5001/" -isu "https://localhost:5003/" -bsu "https://localhost:5009/" -csu "https://localhost:5015/" -tid $tenantId
+    octo-cli -c Config -asu "https://localhost:5001/" -isu "https://localhost:5003/" -bsu "https://localhost:5009/" -csu "https://localhost:5015/" -rsu "https://localhost:5007/" -tid $tenantId
     octo-cli -c Login -i
 }
 
