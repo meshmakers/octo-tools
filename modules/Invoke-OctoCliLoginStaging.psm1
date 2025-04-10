52547fade4449b8f91e5d577c5c08c4ffadd8472
Reimar Klammer <reimar.klammer@meshmakers.io>
2025-04-10 15:15:15 +0200

    AB#1955 Add powershell modules to login to production and staging

---
+++ modules/Invoke-OctoCliLoginStaging.psm1
@@ -0,0 +1,17 @@
+function Invoke-OctoCliLoginLocal {
+    param($tenantId = "meshtest", $includeReporting = $false)
+
+    if ($includeReporting) {
+        Write-Host "Including reporting"
+        octo-cli -c Config -asu "https://assets.staging.meshmakers.cloud/" -isu "https://connect.staging.meshmakers.cloud" -bsu "https://bots.staging.meshmakers.cloud/" -csu "https://communication.staging.meshmakers.cloud/" -rsu "https://reporting.staging.meshmakers.cloud/"  -tid $tenantId
+    }
+    else {
+        Write-Host "Excluding reporting"
+        octo-cli -c Config -asu "https://assets.staging.meshmakers.cloud/" -isu "https://connect.staging.meshmakers.cloud" -bsu "https://bots.staging.meshmakers.cloud/" -csu "https://communication.staging.meshmakers.cloud/" -tid $tenantId
+    }
+
+    octo-cli -c Login -i
+}
+
+Export-ModuleMember -Function @('Invoke-OctoCliLoginLocal')
+
