f43a0d5dfd9a88ce55a5aa63d581ba514a8d9b8e
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-04-21 15:37:06 +0200

    AB#1915: New: Changed api scopes for asset repo

--- modules/Invoke-SetDebugConfiguration.psm1
+++ modules/Invoke-SetDebugConfiguration.psm1
@@ -3,7 +3,8 @@ function Invoke-SetDebugConfiguration
     # Ensure that you have logged in to identity services (Invoke-OctoCliLoginLocal)
 
     octo-cli -c AddAuthorizationCodeClient --clienturi https://localhost:44483/ --clientid octo-admin-panel-debug --redirectUri "https://localhost:44483/" --name "Admin Panel debug"
-    octo-cli -c AddScopeToClient --clientid octo-admin-panel-debug --name "systemAPI.full_access"
+    octo-cli -c AddScopeToClient --clientid octo-admin-panel-debug --name "assetSystemAPI.full_access"
+    octo-cli -c AddScopeToClient --clientid octo-admin-panel-debug --name "assetTenantAPI.full_access"
     octo-cli -c AddScopeToClient --clientid octo-admin-panel-debug --name "identityAPI.full_access"
     octo-cli -c AddScopeToClient --clientid octo-admin-panel-debug --name "botAPI.full_access"
     octo-cli -c AddScopeToClient --clientid octo-admin-panel-debug --name "communicationSystemAPI.full_access"
