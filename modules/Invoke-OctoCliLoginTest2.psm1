7b404f1f03da26311ab0bd50300b742ca4f8175e
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-07-06 13:48:29 +0200

    AB#2259: Update to new test-2 cluster

---
+++ modules/Invoke-OctoCliLoginTest2.psm1
@@ -0,0 +1,17 @@
+function Invoke-OctoCliLoginLocal1 {
+    param($tenantId = "meshtest", $includeReporting = $false)
+
+    if ($includeReporting) {
+        Write-Host "Including reporting"
+        octo-cli -c Config -asu "https://assets.test-2.mm.cloud/" -isu "https://connect.test-2.mm.cloud/" -bsu "https://bots.test-2.mm.cloud/" -csu "https://communication.test-2.mm.cloud/" -rsu "https://reporting.test-2.mm.cloud/" -tid $tenantId
+    }
+    else {
+        Write-Host "Excluding reporting"
+        octo-cli -c Config -asu "https://assets.test-2.mm.cloud/" -isu "https://connect.test-2.mm.cloud/" -bsu "https://bots.test-2.mm.cloud/" -csu "https://communication.test-2.mm.cloud/" -tid $tenantId
+    }
+
+    octo-cli -c Login -i
+}
+
+Export-ModuleMember -Function @('Invoke-OctoCliLoginTest2')
+
