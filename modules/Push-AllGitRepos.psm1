12b9a400dccbdd53c5aba43c0e4a1e50a338ea82
Gerald Lochner <gerald.lochner@meshmakers.io>
2026-01-21 11:32:30 +0100

    AB#3205: New: Push all git repos with branch parameter.

--- modules/Push-AllGitRepos.psm1
+++ modules/Push-AllGitRepos.psm1
@@ -1,18 +1,27 @@
 
 function Push-AllGitRepos
 {
-    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Low')]    
-    param($commitMessage = "Updated")
-    
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Low')]
+    param(
+        [string]$commitMessage = "Updated",
+        [string]$branch = ""
+    )
+
     if (!(Test-Path $rootPath)) {
         Write-Error "Root path $rootPath does not exist"
         return;
     }
-    
-    
+
+    $branchRootPath = Join-Path -Path $rootPath -ChildPath $branch
+
+    if (!(Test-Path $branchRootPath)) {
+        Write-Error "Branch path $branchRootPath does not exist"
+        return;
+    }
+
     # Get all directories starting with "octo-" and "mm-""
-    $allDirectories = Get-ChildItem -Directory -Path $rootPath -Filter "octo-*"
-    $allDirectories += Get-ChildItem -Directory -Path $rootPath -Filter "mm-*"
+    $allDirectories = Get-ChildItem -Directory -Path $branchRootPath -Filter "octo-*"
+    $allDirectories += Get-ChildItem -Directory -Path $branchRootPath -Filter "mm-*"
 
     foreach ($directory in $allDirectories) {
         $gitDirectory = Join-Path -Path $directory.FullName -ChildPath ".git"
